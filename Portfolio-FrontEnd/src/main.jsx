743e12c6cc8aeee914fab2285b031eb74aa65fd7
Pedro Almeida <pedroh_7@hotmail.com>
2024-01-24 19:27:20 -0300

    feat: Delete styling and add theme

--- Portfolio-FrontEnd/src/main.jsx
+++ Portfolio-FrontEnd/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#444466',
+      main: '#222244',
+      dark: '#111133',
+    },
+    secondary: {
+      light: '#FFEECC',
+      main: '#FF5522',
+    },
+    error: {
+      main: '#DD0000',
+    },
+    warning: {
+      main: '#FF8833',
+    },
+    success: {
+      main: '#118822',
+    },
+    info: {
+      main: '#315FCE',
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
