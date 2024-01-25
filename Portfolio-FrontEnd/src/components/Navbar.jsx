d4e8baa2345e257b98dc6ca388be781b21410b45
Pedro Almeida <pedroh_7@hotmail.com>
2024-01-24 22:12:31 -0300

    feat: Add Navbar responsivity for mobile

--- Portfolio-FrontEnd/src/components/Navbar.jsx
+++ Portfolio-FrontEnd/src/components/Navbar.jsx
@@ -3,8 +3,21 @@ import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleOpen = (e) => {
+    setAnchorEl(e.currentTarget);
+  }
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
 
   return (
     <AppBar 
@@ -14,12 +27,12 @@ const Navbar = () => {
         flexDirection: 'row', 
         alignItems: 'center',
         justifyContent: 'space-between',
-        padding: '16px 30px',
+        padding: {xs: '12px 24px', md: '16px 30px'},
         height: '73px',
         }}>
 
       <Box sx={{
-        display: 'flex',
+        display: {xs: 'none', md: 'flex'},
         flexDirection: 'row',
         gap: '100px',
         alignItems: 'center',
@@ -42,6 +55,49 @@ const Navbar = () => {
           <Link href="#" underline="none" color="inherit">
             Descobrir
           </Link>
+        </Box>
+      </Box>
+
+      <Box sx={{
+        display: {xs: 'flex', md: 'none'},
+        flexDirection: 'row',
+        gap: '6px',
+        alignItems: 'center',
+      }}>
+        <IconButton id="menu-button" color="inherit"
+          aria-label="Open menu" 
+          aria-controls={open ? 'nav-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+        
+        <Menu
+          id="nav-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": 'menu-button',
+          }}
+          sx={{fontSize: '16px'}}
+        >
+          <MenuItem onClick={handleClose} sx={{'&:hover': {backgroundColor: 'secondary.light'}}}>
+            <Link href="#" underline="none" color="inherit">
+              Meus Projetos
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose} sx={{'&:hover': {backgroundColor: 'secondary.light'}}}>
+            <Link href="#" underline="none" color="inherit">
+              Descobrir
+            </Link>
+          </MenuItem>
+        </Menu>
+
+        <Box sx={{height: '41px'}}>
+          <img src="src/assets/orange-portfolio-logo.png" alt="Logo do Orange Portfólio" />
         </Box>
       </Box>
         
