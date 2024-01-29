fa7e50dc894b42d94f747595cbd4a95873e11b13
dgomesdev <danilo.gomes@outlook.fr>
2024-01-29 08:39:49 -0400

    fix: Correcao de um bug do tema/adiçao dos valores main, light e dark para cada cor

--- Portfolio-FrontEnd/src/main.jsx
+++ Portfolio-FrontEnd/src/main.jsx
@@ -1,82 +1,77 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { ThemeProvider, createTheme } from "@mui/material";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#EDEFF2',
-      colorPrincipal70: '#EDEFF2',
-      colorPrincipal80: '#444466',
-      colorPrincipal90: '#222244',
-      colorPrincipal100: '#111133',
+      contrastText: '#EDEFF2',
+      light: '#444466',
+      main: '#222244',
+      dark: '#111133',
     },
     secondary: {
-      main: '#FFEECC',
-      colorSecondary60: '#FFEECC',
-      colorSecondary70: '#FFCC99',
+      contrastText: '#FFEECC',
+      light: '#FFCC99',
       colorSecondary80: '#FFAA66',
       colorSecondary90: '#FF8833',
-      colorSecondary100: '#FF5522',
+      main: '#FF5522',
       colorSecondary110: '#CC4400',
       colorSecondary120: '#993300',
-      colorSecondary130: '#662200',
+      dark: '#662200',
     },
     neutral: {
-      colorNeutral60: '#FCFDFF',
-      colorNeutral70: '#E6E9F2',
+      contrastText: '#FCFDFF',
+      light: '#E6E9F2',
       colorNeutral80: '#C2C4CC',
       colorNeutral90: '#A1A3AA',
-      colorNeutral100: '#818388',
+      main: '#818388',
       colorNeutral110: '#515255',
       colorNeutral120: '#303133',
-      colorNeutral130: '#0B0C0D',
+      dark: '#0B0C0D',
     },
     success: {
-      main: '#EEFFBB',
-      colorSuccess60: '#EEFFBB',
-      colorSuccess70: '#BBEE88',
+      contrastText: '#EEFFBB',
+      light: '#BBEE88',
       colorSuccess80: '#88CC66',
       colorSuccess90: '#55BB44',
-      colorSuccess100: '#229922',
+      main: '#229922',
       colorSuccess110: '#118822',
       colorSuccess120: '#006622',
-      colorSuccess130: '#004422',
+      dark: '#004422',
     },
-    alert: {
-      colorAlert60: '#FFFFCC',
-      colorAlert70: '#FFEE99',
+    warning: {
+      contrastText: '#FFFFCC',
+      light: '#FFEE99',
       colorAlert80: '#FFEE66',
       colorAlert90: '#FFDD33',
-      colorAlert100: '#FFCC00',
+      main: '#FFCC00',
       colorAlert110: '#CC9900',
       colorAlert120: '#997700',
-      colorAlert130: '#664400',
+      dark: '#664400',
     },
     error: {
-      main: '#FFDDCC',
-      colorError60: '#FFDDCC',
-      colorError70: '#FFAA99',
+      contrastText: '#FFDDCC',
+      light: '#FFAA99',
       colorError80: '#FF7766',
       colorError90: '#FF4433',
-      colorError100: '#DD0000',
+      main: '#DD0000',
       colorError110: '#BB0000',
       colorError120: '#880000',
-      colorError130: '#660000',
+      dark: '#660000',
     },
     info: {
-      main: '#ADCBFA',
-      colorInfo60: '#ADCBFA',
-      colorInfo70: '#82A9F0',
+      contrastText: '#ADCBFA',
+      light: '#82A9F0',
       colorInfo80: '#608AE1',
       colorInfo90: '#315FCE',
-      colorInfo100: '#2348B1',
+      main: '#2348B1',
       colorInfo110: '#183594',
       colorInfo120: '#0F2477',
-      colorInfo130: '#091862',
-    },
+      dark: '#091862',
+    }
   },
   typography: {
     fontFamily: 'Roboto, sans-serif',
@@ -87,7 +82,7 @@ const theme = createTheme({
       lineHeight: '40px' /* 41.667% */,
       letterSpacing: '-1.5px',
       fontFeatureSettings: "'clig' off, 'liga' off",
-      color: '#0B0C0D',
+      color: 'neutral.dark',
     },
     h2: {
       fontSize: '60px',
@@ -95,14 +90,14 @@ const theme = createTheme({
       fontWeight: '300',
       lineHeight: '50px' /* 83.333% */,
       letterSpacing: '-0.5px',
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     h3: {
       fontSize: '48px',
       fontStyle: 'normal',
       fontWeight: '400',
       lineHeight: '40px' /* 83.333% */,
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     h4: {
       fontSize: '34px',
@@ -110,14 +105,14 @@ const theme = createTheme({
       fontWeight: '400',
       lineHeight: '34px' /* 1007% */,
       letterSpacing: '0.25px',
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     h5: {
       fontSize: '24px',
       fontStyle: 'normal',
       fontWeight: '400',
       lineHeight: '24px' /* 100% */,
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     h6: {
       fontSize: '20px',
@@ -125,7 +120,7 @@ const theme = createTheme({
       fontWeight: '500',
       lineHeight: '20px' /* 100% */,
       letterSpacing: '0.15px',
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     subtitle1: {
       fontSize: '16px',
@@ -133,7 +128,7 @@ const theme = createTheme({
       fontWeight: '400',
       lineHeight: '16px' /* 100% */,
       letterSpacing: '0.15px',
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     subtitle2: {
       fontSize: '14px',
@@ -141,7 +136,7 @@ const theme = createTheme({
       fontWeight: '500',
       lineHeight: '24px' /* 171.429% */,
       letterSpacing: '0.1px',
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     label: {
       fontSize: '16px',
@@ -150,7 +145,7 @@ const theme = createTheme({
       lineHeight: '24px' /* 150% */,
       letterSpacing: '0.15px',
       fontFeatureSettings: "'clig' off, 'liga' off",
-      color: '#0B0C0D',
+      color: 'neutral.dark',
     },
     body1: {
       fontSize: '16px',
@@ -158,7 +153,7 @@ const theme = createTheme({
       fontWeight: '400',
       lineHeight: '16px' /* 100% */,
       letterSpacing: '0.5px',
-      color: '#0B0C0D',
+      color: 'neutral.dark',
     },
     body2: {
       fontSize: '14px',
@@ -166,7 +161,7 @@ const theme = createTheme({
       fontWeight: '400',
       lineHeight: '14px' /* 100% */,
       letterSpacing: '0.25px',
-      color: '#0B0C0D',
+      color: 'neutral.dark',
     },
     caption: {
       fontSize: '12px',
@@ -174,7 +169,7 @@ const theme = createTheme({
       fontWeight: '400',
       lineHeight: '16px' /* 133.333% */,
       letterSpacing: '0.4px',
-      color: '#0B0C0D',
+      color: 'neutral.dark',
     },
     overline: {
       fontSize: '10px',
@@ -182,7 +177,7 @@ const theme = createTheme({
       fontWeight: '400',
       lineHeight: '12px' /* 120% */,
       letterSpacing: '1.5px',
-      color: '#0B0C0D'
+      color: 'neutral.dark'
     },
     button: {
       fontSize: '15px',
@@ -196,10 +191,10 @@ const theme = createTheme({
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <App />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
