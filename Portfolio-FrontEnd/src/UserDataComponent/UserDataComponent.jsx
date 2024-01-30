e3296c8fde23bd9720524c78bfbacd8ef838e1a8
dgomesdev <danilo.gomes@outlook.fr>
2024-01-29 21:28:15 -0400

    feat: added user data component for home screen

---
+++ Portfolio-FrontEnd/src/UserDataComponent/UserDataComponent.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+import { Button, Stack, Typography } from "@mui/material";
+import "../../../Portfolio-FrontEnd/assets/userAvatar.png";
+
+function UserDataComponent({ user }) {
+  return (
+    <Stack
+      sx={{
+        flexDirection: "row",
+        bgcolor: "white",
+        gap: "42px",
+      }}
+    >
+      <img
+        src={user.avatar}
+        alt="User image"
+        width="122"
+        style={{
+          borderRadius: "50%",
+        }}
+      />
+
+      <Stack
+        sx={{
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Stack
+          sx={{
+            flexDirection: "row",
+            gap: "16px",
+          }}
+        >
+          <Typography variant="h5">{user.name}</Typography>
+          <Typography variant="h5">{user.lastName}</Typography>
+        </Stack>
+
+        <Typography variant="subtitle1">{user.country}</Typography>
+
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.12)",
+            color: "rgba(0, 0, 0, 0.38)",
+          }}
+        >
+          <Typography variant="button">Adicionar projeto</Typography>
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default UserDataComponent;
