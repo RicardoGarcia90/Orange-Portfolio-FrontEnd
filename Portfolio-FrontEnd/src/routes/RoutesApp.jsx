dd672e61d1b9f0a98964b572e2a66faa1bd7b016
Ricardo Garcia <91982447+RicardoGarcia90@users.noreply.github.com>
2024-01-31 16:22:30 -0300

    Revert "Develop ricardo garcia"

--- Portfolio-FrontEnd/src/routes/RoutesApp.jsx
+++ Portfolio-FrontEnd/src/routes/RoutesApp.jsx
@@ -2,14 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home';
 import Login from '../Pages/Login';
 import MeuPortfolio from '../Pages/MeuPortfolio';
-import Register from '../Pages/Register';
+import AddEditProject from '../AddEditProject/AddEditProject';
 
 const RoutesApp = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/meuportfolio" element={<MeuPortfolio />} />
         <Route path="/home" element={<Home />} />
       </Routes>
