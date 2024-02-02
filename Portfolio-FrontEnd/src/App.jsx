7197bc3592be594d5ef3ed4decfd47cec6bd65a2
Ricardo Garcia <ricardonunesgarcia@outlook.com>
2024-02-02 18:23:42 -0300

    fix: ajustando versionamento

--- Portfolio-FrontEnd/src/App.jsx
+++ Portfolio-FrontEnd/src/App.jsx
@@ -1,7 +1,27 @@
-import RoutesApp from '../src/routes/RoutesApp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import MeuPortfolio from './Pages/MeuPortfolio';
+import Register from './Pages/Register';
 
-function App() {
-  return <RoutesApp></RoutesApp>;
-}
+import UserContext from './contexts/UserContext';
+import { useState } from 'react';
+
+const App = () => {
+  const [user, setUser] = useState({});
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/meuportfolio" element={<MeuPortfolio />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </UserContext.Provider>
+  );
+};
 
 export default App;
